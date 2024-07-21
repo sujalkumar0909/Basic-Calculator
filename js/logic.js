4bfd532ccc834a9da5fd433f81552feb6ba34977
Sujal <sujalkumar7209@gmail.com>
2024-07-21 10:24:01 +0530

    new file:   css/style.css 	new file:   index.html 	new file:   js/logic.js

---
+++ js/logic.js
@@ -0,0 +1,31 @@
+let string="";
+let buttons=document.querySelectorAll("button");
+let q=document.querySelector("input");
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+    if(e.target.innerHTML=='=')
+        {
+            string=eval(string);
+            q.value=string;
+        }
+        else if(e.target.innerHTML=='AC')
+            {
+                string="";
+                q.value=string;
+            }
+            else if(e.target.innerHTML=='+/-')
+                {
+                    string='-'+'('+string+')';
+                    q.value=string;
+                }
+                else if(e.target.innerHTML=='DEL')
+                    {
+                        string=string.slice(0,-1);
+                        q.value=string;
+                    }
+        else{
+            string=string+e.target.innerHTML;
+            q.value=string;
+        }
+})
+})
